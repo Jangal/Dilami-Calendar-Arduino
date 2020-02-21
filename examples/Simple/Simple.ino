b7ddc54d7ab8a37f86cf2c79087fd20e267805a7
Arma <Arma@Jangal.co>
2020-02-21 09:07:35 +0330

    initialization

---
+++ examples/Simple/Simple.ino
@@ -0,0 +1,18 @@
+#include <Dilami.h>
+
+DilamiCalendar dc(1398, 12, 2);
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Dilami Day: ");
+  Serial.println(dc.getDilamiDay());
+  Serial.print("Dilami Month: ");
+  Serial.println(dc.getDilamiMonth());
+  Serial.print("Dilami Year: ");
+  Serial.println(dc.getDilamiYear());
+  delay(5000);        // repeat after 5 seconds.
+}
